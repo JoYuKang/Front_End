ca4091e11964743b6f3461a1686e6974c3c455aa
YU KANG JO <46198324+JoYuKang@users.noreply.github.com>
2022-01-21 21:19:58 +0900

    # header Css 수정

---
+++ starbucks/js/main.js
@@ -0,0 +1,21 @@
+
+const searchEl = document.querySelector('.search');
+const searchInputEl = searchEl.querySelector('input');
+
+console.log(searchEl)
+console.log(searchInputEl)
+
+searchEl.addEventListener('click', function(){
+  searchInputEl.focus();
+})
+
+searchInputEl.addEventListener('focus',function(){
+  searchEl.classList.add('focused');
+  searchInputEl.setAttribute('placeholder','통합검색');
+})
+
+
+searchInputEl.addEventListener('blur',function(){
+  searchEl.classList.remove('focused');
+  searchInputEl.setAttribute('placeholder','');
+})
