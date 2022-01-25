5421de9d64ce9ee2a9dcc6becaad9d68974dee09
YU KANG JO <46198324+JoYuKang@users.noreply.github.com>
2022-01-25 22:17:20 +0900

    # Main Image 추가

--- starbucks/js/main.js
+++ starbucks/js/main.js
@@ -1,21 +1,51 @@
-
 const searchEl = document.querySelector('.search');
 const searchInputEl = searchEl.querySelector('input');
 
-console.log(searchEl)
-console.log(searchInputEl)
+// console.log(searchEl)
+// console.log(searchInputEl)
 
-searchEl.addEventListener('click', function(){
+searchEl.addEventListener('click', function () {
   searchInputEl.focus();
 })
 
-searchInputEl.addEventListener('focus',function(){
+searchInputEl.addEventListener('focus', function () {
   searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder','통합검색');
+  searchInputEl.setAttribute('placeholder', '통합검색');
 })
 
 
-searchInputEl.addEventListener('blur',function(){
+searchInputEl.addEventListener('blur', function () {
   searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder','');
+  searchInputEl.setAttribute('placeholder', '');
 })
+
+
+// _.throttle(함수, 시간 )
+const badgeEl = document.querySelector('header .badges');
+window.addEventListener('scroll', _.throttle(function () {
+  if (window.scrollY > 500) {
+    //gsap.to(요소, 지속시간, 옵션)
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    });
+
+  } else {
+    //gsap.to(요소, 지속시간, 옵션)
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block  '
+    });
+  }
+}, 300));
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function (fadeEl, index) {
+  //gsap.to(요소, 지속시간, 옵션)
+  gsap.to(fadeEl, 1, {
+    delay: (index +1) * .7,
+    opacity : 1
+  });
+});
+
+console.log(fadeEls);
