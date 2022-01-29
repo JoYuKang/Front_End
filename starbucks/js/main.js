b019b519c1aaa10ae785fe035df627cbe5357707
YU KANG JO <46198324+JoYuKang@users.noreply.github.com>
2022-01-29 21:09:09 +0900

    # 스타벅스 완성

--- starbucks/js/main.js
+++ starbucks/js/main.js
@@ -20,24 +20,50 @@ searchInputEl.addEventListener('blur', function () {
 })
 
 
-// _.throttle(함수, 시간 )
-const badgeEl = document.querySelector('header .badges');
+/**
+ * 페이지 스크롤에 따른 요소 제어
+ */
+// 페이지 스크롤에 영향을 받는 요소들을 검색!
+const badgeEl = document.querySelector('header .badges')
+const toTopEl = document.querySelector('#to-top')
+// 페이지에 스크롤 이벤트를 추가!
+// 스크롤이 지나치게 자주 발생하는 것을 조절(throttle, 일부러 부하를 줌)
 window.addEventListener('scroll', _.throttle(function () {
+  // 페이지 스크롤 위치가 500px이 넘으면.
   if (window.scrollY > 500) {
-    //gsap.to(요소, 지속시간, 옵션)
+    // Badge 요소 숨기기!
     gsap.to(badgeEl, .6, {
       opacity: 0,
       display: 'none'
-    });
+    })
+    // 상단으로 스크롤 버튼 보이기!
+    gsap.to(toTopEl, .2, {
+      x: 0
+    })
 
+    // 페이지 스크롤 위치가 500px이 넘지 않으면.
   } else {
-    //gsap.to(요소, 지속시간, 옵션)
+    // Badge 요소 보이기!
     gsap.to(badgeEl, .6, {
       opacity: 1,
-      display: 'block  '
-    });
+      display: 'block'
+    })
+    // 상단으로 스크롤 버튼 숨기기!
+    gsap.to(toTopEl, .2, {
+      x: 100
+    })
   }
-}, 300));
+}, 300))
+// 상단으로 스크롤 버튼을 클릭하면,
+toTopEl.addEventListener('click', function () {
+  // 페이지 위치를 최상단으로 부드럽게(0.7초 동안) 이동.
+  gsap.to(window, .7, {
+    scrollTo: 0
+  })
+})
+
+
+
 
 const fadeEls = document.querySelectorAll('.visual .fade-in');
 fadeEls.forEach(function (fadeEl, index) {
@@ -78,6 +104,18 @@ var swiper1 = new Swiper(".promotion .swiper-container", {
   }
 
 });
+
+var swiper2 = new Swiper(".awards .swiper-container", {
+  autoplay: true,
+  loop: true,
+  slidesPerView: 5,
+  spaceBetween: 30,
+  navigation: {
+    prevEl: '.awards .swiper-prev',
+    nextEl: '.awards .swiper-next'
+  }
+});
+
 
 const promotionEl = document.querySelector('.promotion ')
 
@@ -138,6 +176,10 @@ spyEls.forEach(function (spyEl) {
       triggerElement: spyEl, // 보여짐 여부를 요소를 지정
       triggerHook: .8
     })
-    .setClassToggle(spyEl,'show')
+    .setClassToggle(spyEl, 'show')
     .addTo(new ScrollMagic.Controller());
 })
+
+
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear();
