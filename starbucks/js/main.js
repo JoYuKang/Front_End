926ca7c895bf378b98380780732a6bc7f457d700
YU KANG JO <46198324+JoYuKang@users.noreply.github.com>
2022-02-01 17:15:43 +0900

    # 공통 모듈 분리

--- starbucks/js/main.js
+++ starbucks/js/main.js
@@ -1,23 +1,4 @@
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
 
-// console.log(searchEl)
-// console.log(searchInputEl)
-
-searchEl.addEventListener('click', function () {
-  searchInputEl.focus();
-})
-
-searchInputEl.addEventListener('focus', function () {
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색');
-})
-
-
-searchInputEl.addEventListener('blur', function () {
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
-})
 
 
 /**
@@ -180,6 +161,3 @@ spyEls.forEach(function (spyEl) {
     .addTo(new ScrollMagic.Controller());
 })
 
-
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear();
