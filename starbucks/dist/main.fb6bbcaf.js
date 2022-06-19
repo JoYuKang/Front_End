3fad61dcfbc1403ad636e0c9286bd1202268f2a9
YU KANG JO <46198324+JoYuKang@users.noreply.github.com>
2022-06-20 00:04:56 +0900

    # gh-pages -d build

--- starbucks/dist/main.fb6bbcaf.js
+++ starbucks/dist/main.fb6bbcaf.js
@@ -285,7 +285,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52875" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53468" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
