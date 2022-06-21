f96d063f41f12edee0541e201c3d497077998b11
YU KANG JO <46198324+JoYuKang@users.noreply.github.com>
2022-06-21 22:21:28 +0900

    # SCSS 연습

--- scc_practice/dist/sub.39b32afd.js
+++ scc_practice/dist/sub.39b32afd.js
@@ -184,7 +184,7 @@ function reloadCSS() {
 }
 
 module.exports = reloadCSS;
-},{"./bundle-url":"node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"main.scss":[function(require,module,exports) {
+},{"./bundle-url":"node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"sub.scss":[function(require,module,exports) {
 var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
@@ -394,4 +394,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
-//# sourceMappingURL=/main.39afc03c.js.map
+//# sourceMappingURL=/sub.39b32afd.js.map
